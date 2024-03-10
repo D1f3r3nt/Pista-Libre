c40b173156de0de49dd22096e77eb5bd39c2c77d
D1f3r3nt <98466831+D1f3r3nt@users.noreply.github.com>
2024-03-10 17:36:42 +0100

    Initial commit

---
+++ File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  pista-libre-back
+//
+//  Created by Marc Santisteban Ruiz on 10/3/24.
+//
+
+import Foundation
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,15 +7,14 @@ import Vapor
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
-    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
-        tls: .prefer(try .init(configuration: .clientDefault)))
-    ), as: .psql)
+    
+    guard let _ = Environment.process.API_KEY else { fatalError("API_KEY not found") }
+    guard let databaseURL = Environment.process.DATABASE_URL else { fatalError("DATABASE_URL not found") }
+    
+    app.databases.use(
+        try DatabaseConfigurationFactory.postgres(url: databaseURL),
+        as: .psql
+    )
 
     app.migrations.add(CreateTodo())
     // register routes
