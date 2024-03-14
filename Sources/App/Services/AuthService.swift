032c0be32519092732c300dcd91a287bde55113e
D1f3r3nt <98466831+D1f3r3nt@users.noreply.github.com>
2024-03-14 23:26:25 +0100

    PIS-7: Add login

--- Sources/App/Services/AuthService.swift
+++ Sources/App/Services/AuthService.swift
@@ -11,13 +11,36 @@ class AuthService {
             try await newUser.create(on: req.db)
             
         } catch let error {
-            return Response(
-                status: .badRequest,
-                body: Response.Body(stringLiteral: "Service Exception: \(error)")
+            throw Abort(
+                .badRequest,
+                reason: "Service Exception: \(error)"
             )
         }
         
         return Response(status: .created)
     }
     
+    func logIn(req: Request) async throws -> Response {
+        
+        guard let email: String = req.query["email"] else {
+            throw Abort(.badRequest, reason: "Email missed")
+        }
+        
+        guard let password: String = req.query["password"] else {
+            throw Abort(.badRequest, reason: "Password missed")
+        }
+        
+        guard let user: User = try await User.query(on: req.db)
+            .filter(\.$email == email)
+            .first()
+        else {
+            throw Abort(.badRequest, reason: "Email dosen't exist")
+        }
+        
+        if (user.password != password) {
+            throw Abort(.badRequest, reason: "Incorrect password")
+        }
+        
+        return Response(status: .ok)
+    }
 }
